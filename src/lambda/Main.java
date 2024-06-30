0fbff22641e7c4c7b96dc95c25a8fbcad7aac78e
quid-0 <wodnd101@naver.com>
2024-06-30 17:21:11 +0900

    클래스명 변경

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static behaviorParameterization.Color.GREEN;
 import static behaviorParameterization.Color.RED;
 
-public class Lambda {
+public class Main {
 
     public static void main(String[] args) {
 
